09d46b5e9af29235485ff9b173a46ca44bec08a1
Frank <frank@beekmanmedia.nl>
2022-11-24 18:11:52 +0100

    Added table components

---
+++ packages/ui/atoms/column/column.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Column } from "./column";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Atoms/Column",
+  component: Column,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+  },
+  parameters: {
+    status: {
+      type: [
+        "building",
+      ],
+    },
+  },
+} as any;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Column> = (args) => <Column {...args} />;
+
+export const Primary: ComponentMeta<typeof Column> = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  value: "Column",
+};
---
+++ packages/ui/molecules/row/row.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export interface TableRowProps {
+  style?: object
+  bg?: string
+  text?: string
+  weight?: string
+  children: any
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Row = ({
+                         style = {},
+                         bg = "surface",
+                         text = "onSurface",
+                         weight = "normal",
+                         children,
+                         ...props
+                       }: TableRowProps) => {
+  return (
+    <tr
+      style={style}
+      className={[
+        "relative",
+        "border-surfaceDark",
+        "table-cell",
+        "tableColumn",
+        "p-4",
+        bg ? `bg-${bg}` : "",
+        text ? `text-${text}` : "",
+        `font-${weight}`,
+      ].join(" ")}
+      {...props}
+    >
+      {children}
+    </tr>
+  );
+};
