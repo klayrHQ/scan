bae95c3b1b558e458a7c09fa8b1b4c5e3324a3de
corbifex <sander@moosty.com>
2023-05-28 18:05:43 +0200

    update

--- apps/web/components/data/table/columns/grid.tsx
+++ apps/web/components/data/table/columns/grid.tsx
@@ -4,9 +4,9 @@ import { ColumnProps } from "./index";
 export const GridColumn = ({ values }: ColumnProps) => {
   const rows = Array(Math.ceil(values.length / 2), 1);
   return (
-    <div className={"flex"}>
+    <div>
       {rows.map((v, i) => (
-        <div className={"flex"}>
+        <div className={"flex flex-row"}>
           {values.slice(i * 2, 2 + (i * 2)).map((value, index) => (
             <div className={`${index % 2 === 1 ? "grow" : ""}`}>
               <ValueFormatter
