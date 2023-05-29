47a1a06fa8df95d16bca4573b55450e5bb67b484
RaphaelCornelis <raphael@moosty.com>
2023-05-29 19:04:37 +0200

    status added

--- apps/web/components/data/table/columns/validatorStatus.tsx
+++ apps/web/components/data/table/columns/validatorStatus.tsx
@@ -3,9 +3,9 @@ import {cls, Typography} from "ui";
 import {ValueFormatter} from "../../../valueFormatter";
 
 export const ValidatorStatusColumn = ({params, values}: ColumnProps) => {
-  return <span className={"flex space-x-2 bg-surface-1 p-2 rounded"}>
+  return <span className={"flex space-x-2 bg-surface-1 p-2 rounded items-center"}>
     {<ValueFormatter value={values[0].value} {...values[0].format} />}
-    {<ValueFormatter value={values[1].value} {...values[1].format} />}
+    {values[0].value && values[0].value === "active" ? <ValueFormatter value={values[1].value} {...values[1].format} /> : <ValueFormatter value={values[2].value} {...values[2].format} />}
   </span>
 // <Typography
 //   tag={"span"}
