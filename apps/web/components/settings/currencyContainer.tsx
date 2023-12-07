66e1a4b52f8225fed35f0d4635308cc6b5759510
corbifex <sander@moosty.com>
2023-12-07 14:10:35 +0100

    fix error

--- apps/web/components/settings/currencyContainer.tsx
+++ apps/web/components/settings/currencyContainer.tsx
@@ -19,7 +19,7 @@ export const CurrencyContainer = () => {
         <Typography tag={"span"}>{"Format the way Liskscan shows all currency values."}</Typography>
       </Paper>
       <div className="bg-surface-1 text-onSurfaceHigh w-full block rounded">
-        <Decimals minMax={{min: 0, max:8}} setSetting={setSetting} switchConvert={switchConvert} parsedSettings={parsedSettings}/>
+        <Decimals currencies={[]} minMax={{min: 0, max:8}} setSetting={setSetting} switchConvert={switchConvert} parsedSettings={parsedSettings}/>
       </div>
       <div className="bg-surface-0 text-onSurfaceHigh flex flex-col space-y-4 ">
         {/*<h1 className="font-bold text-xl text-onSurfaceHigh">*/}
