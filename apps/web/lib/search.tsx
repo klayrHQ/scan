44ef50f1046a21fe95def0c53eea3b1c14d091fd
corbifex <sander@moosty.com>
2023-12-07 14:17:46 +0100

    fix error

--- apps/web/lib/search.tsx
+++ apps/web/lib/search.tsx
@@ -27,6 +27,7 @@ export const search = async (
           {
             value: (
               <SearchResult
+                setOpen={setOpen}
                 saveSearch={saveSearch}
                 menuCloseFunction={menuCloseFunction}
                 type={"account"}
