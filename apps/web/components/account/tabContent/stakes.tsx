dd65c799641a4cebe4013274a40ce5999e9410e4
Franka <frank@beekmanmedia.nl>
2023-12-18 17:46:17 +0100

    add no incoming/outgoing stakes when tabs are empty

--- apps/web/components/account/tabContent/stakes.tsx
+++ apps/web/components/account/tabContent/stakes.tsx
@@ -1,5 +1,7 @@
 import {TableSlice} from "../../../slices/table";
 import {stakersColumns, stakesColumns} from "../table/columns/stakesColumns";
+import {Grid, Typography} from "ui";
+import {ConsoleLogTester} from "../../consoleLogTester";
 
 export const Stakes = ({
   queryData,
@@ -8,10 +10,16 @@ export const Stakes = ({
   queryData: any
   tab: string
 }) => {
+  const stakes = queryData["account-stakes-sent"]
+  const stakers = queryData["account-received-stakes"]
 
-  return (
+  return (tab === "stakes" && stakes?.meta?.count <= 0) || (tab === "stakers" && stakers?.meta?.count <= 0) ? (
+    <Grid className={"m-8"} flex>
+      <Typography tag={"span"}>{tab === "stakes" ? "No outgoing stakes" : "No incoming stakes"}</Typography>
+    </Grid>
+  ) : (
     <TableSlice
-      queryData={{stakes: queryData["account-stakes-sent"], stakers: queryData["account-received-stakes"]}}
+      queryData={{stakes: stakes, stakers: stakers}}
       table={{
         key: tab === "stakers" ? "stakers" : "stakes",
         columns: tab === "stakers" ? stakersColumns : stakesColumns,
